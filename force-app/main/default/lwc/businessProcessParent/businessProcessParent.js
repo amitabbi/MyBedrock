fd09d6893c5f9a1b29b5ad161cfc6057575038c2
amitabbi <45029620+amitabbi@users.noreply.github.com>
2024-03-16 11:42:41 +1100

    Rn 006 (#10)
    * Batch Class Change
    
    * RetrieveFlowsPackage.xml
    
    * IncludedFlows

---
+++ force-app/main/default/lwc/businessProcessParent/businessProcessParent.js
@@ -0,0 +1,17 @@
+import { LightningElement,api } from 'lwc';
+
+export default class BusinessProcessParent extends LightningElement {
+
+    //@api viewobjectname;
+    createobjectname = "Product2";
+    updateobjectname = "Quote";
+
+    //@api objectApiName;
+
+/*
+    Schema.DescribeSObjectResult R = Account.SObjectType.getDescribe();
+for(Schema.ChildRelationship cr: R.getChildRelationships()){
+    system.debug('Relationship Name =>'+cr.getRelationshipName());
+}*/
+
+}
